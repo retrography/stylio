5d4e026f76cca53e88f50e1723ecfac30bc437a1
Mahmood Shafeie Zargar <mahmood@gmail.com>
2016-05-23 17:54:05 -0400

    updated unpack.sh

--- pack.sh
+++ pack.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-rm -f zotero-stylio.xpi
-apikey=`cat api.key`
-marker="YOUR_API_KEY"
+# Assumes the api-key is 32 characters long or more
 
-sed -i -e "s/$marker/$apikey/" './chrome/content/styliozotero/stylio.js'
-zip -r zotero-stylio.xpi chrome chrome.manifest install.rdf
-sed -i -e "s/$apikey/$marker/" './chrome/content/styliozotero/stylio.js'
-rm -f './chrome/content/styliozotero/stylio.js-e'
+if ! [[ "$(wc -m api.key | sed -E 's/.*(^| |\t)([0-9]+)( |\t).*/\2/')" -ge 32 ]]; then
+	echo 'api.key missing or empty. You need an API Key to run this software. In order to obtain one visit AnyStyle.io website. For more instructions see README.md.'
+	exit
+fi
+
+marker="YOUR_API_KEY"
+apikey=`cat api.key`
+
+sed -i '' -e "s/$marker/$apikey/" './chrome/content/styliozotero/stylio.js'
+zip -FSr zotero-stylio.xpi chrome chrome.manifest install.rdf
+sed -i '' -e "s/$apikey/$marker/" './chrome/content/styliozotero/stylio.js'
