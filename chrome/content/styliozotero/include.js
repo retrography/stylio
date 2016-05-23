9d3842ca26a8e72603d8a6a1a7f7830beabf8f1c
Mahmood Shafeie Zargar <mahmood@gmail.com>
2016-05-23 16:50:05 -0400

    Version 0.1.0 - Proof of concept

--- chrome/content/styliozotero/include.js
+++ chrome/content/styliozotero/include.js
@@ -1,6 +1,6 @@
 // Only create main object once
-if (!Zotero.HelloWorldZotero) {
+if (!Zotero.StylioZotero) {
 	let loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
 					.getService(Components.interfaces.mozIJSSubScriptLoader);
-	loader.loadSubScript("chrome://helloworldzotero/content/hello.js");
+	loader.loadSubScript("chrome://styliozotero/content/stylio.js");
 }
--- pack.sh
+++ pack.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-rm -f zotero-hello-world.xpi
+rm -f zotero-stylio.xpi
 apikey=`cat api.key`
 marker="YOUR_API_KEY"
 
-sed -i -e "s/$marker/$apikey/" './chrome/content/helloworldzotero/hello.js'
-zip -r zotero-hello-world.xpi chrome chrome.manifest install.rdf
-sed -i -e "s/$apikey/$marker/" './chrome/content/helloworldzotero/hello.js'
+sed -i -e "s/$marker/$apikey/" './chrome/content/styliozotero/stylio.js'
+zip -r zotero-stylio.xpi chrome chrome.manifest install.rdf
+sed -i -e "s/$apikey/$marker/" './chrome/content/styliozotero/stylio.js'
+rm -f './chrome/content/styliozotero/stylio.js-e'
